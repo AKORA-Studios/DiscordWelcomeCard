bfeb1688cd7dacf40d384a0e2a38120e0822542a
Maf <dragoncatm@gmail.com>
2021-01-25 20:25:10 +0100

    👌 Switched to module System

--- index.ts
+++ index.ts
@@ -1,14 +1,14 @@
 import { GuildMember, MessageAttachment } from "discord.js";
-import { createCanvas, loadImage } from 'canvas';
+import { createCanvas, loadImage, CanvasRenderingContext2D, Canvas } from 'canvas';
 import path from 'path';
 
-interface Theme {
+export interface Theme {
     name: string;
     color: string;
     image: string;
 }
 
-const themeArray = [
+const themes: Theme[] = [
     { name: 'dark', color: '#ffffff', image: 'dark.png' },
     { name: 'sakura', color: '#7d0b2b', image: 'sakura.png' },
     { name: 'blue', color: '#040f57', image: 'blue.png' },
@@ -17,16 +17,11 @@ const themeArray = [
     { name: 'code', color: '#ffffff', image: 'code.png' },
 ]
 
-const themeMap = new Map()
-themeArray.forEach(t => {
-    themeMap.set(t.name, { color: t.color, image: t.image })
-})
-
 /** 
  * @param {string | Buffer} theme 
  */
 function theme2Img(theme: string) {
-    let canvasTheme = themeMap.get(theme.toLowerCase())
+    let canvasTheme = themes.find(t => t.name === theme)
     //if (!canvasTheme) throw 'Invalid theme! Use: ' + themeArray.map(v => v.name).join(' | ');
 
     if (canvasTheme) return loadImage(path.join(__dirname, 'images', canvasTheme.image))
@@ -36,44 +31,70 @@ function theme2Img(theme: string) {
 }
 
 
+
+
+
+
+
+
+export const welcomeText = (ctx: CanvasRenderingContext2D, canvas: Canvas) => {
+    ctx.font = '30px sans-serif';
+    //ctx.fillStyle = theme.color
+    ctx.fillText(`Welcome to this server,`, canvas.width / 2.7, canvas.height / 3.5);
+}
+
+export const goodbyeText = (ctx: CanvasRenderingContext2D, canvas: Canvas) => {
+    ctx.font = '30px sans-serif';
+    //ctx.fillStyle = theme.color
+    ctx.fillText(`Goodbye,`, canvas.width / 2.7, canvas.height / 3.5);
+}
+
+export const userText = (ctx: CanvasRenderingContext2D, canvas: Canvas, member: GuildMember) => {
+    ctx.font = `${getFontSize(member.user.tag)}px sans-serif`
+    //ctx.fillStyle = canvasTheme.color
+    ctx.fillText(`${member.user.tag}!`, canvas.width / 2.7, canvas.height / 1.8);
+}
+
+export const memberCount = (ctx: CanvasRenderingContext2D, canvas: Canvas, member: GuildMember) => {
+    ctx.font = '24px sans-serif'
+    //ctx.fillStyle = canvasTheme.color
+    ctx.fillText(`MemberCount: ${member.guild.memberCount}`, canvas.width / 2.7, canvas.height / 1.3);
+}
+
+export const avatarImg = async (ctx: CanvasRenderingContext2D, canvas: Canvas, member: GuildMember) => {
+    ctx.lineWidth = 6
+    ctx.beginPath();
+    ctx.arc(canvas.height / 2, canvas.height / 2, canvas.height / 2.5, 0, Math.PI * 2, true);
+    ctx.closePath();
+    ctx.clip();
+
+    ctx.drawImage(await loadImage(member.user.displayAvatarURL({ format: 'png' })), 25, 25, 200, 200)
+}
+
 /**
  * @param {GuildMember} member The GuildMember that joined the Guild.
  * @param {string} theme Theme of the card, this is optional
  */
 export async function welcomeImage(member: GuildMember, theme = 'sakura') {
-    let canvasTheme = themeArray.find(t => t.name === theme.toLowerCase())
-    if (!canvasTheme) throw 'Invalid theme! Use: ' + themeArray.map(v => v.name).join(' | ');
+    let canvasTheme = themes.find(t => t.name === theme.toLowerCase())
+    if (!canvasTheme) throw 'Invalid theme! Use: ' + themes.map(v => v.name).join(' | ');
 
     const canvas = createCanvas(700, 250)
     const ctx = canvas.getContext('2d')
 
     const background = await theme2Img(theme);
-    const avatar = await loadImage(member.user.displayAvatarURL({ format: 'png' }))
 
     ctx.drawImage(background, 0, 0)
-
     ctx.strokeRect(0, 0, canvas.width, canvas.height);
 
-    ctx.font = '30px sans-serif';
-    ctx.fillStyle = canvasTheme.color
-    ctx.fillText(`Welcome to this server,`, canvas.width / 2.7, canvas.height / 3.5);
+    ctx.fillStyle = canvasTheme.color;
+    ctx.strokeStyle = canvasTheme.color;
 
-    ctx.font = `${getFontSize(member.user.tag)}px sans-serif`
-    ctx.fillStyle = canvasTheme.color
-    ctx.fillText(`${member.user.tag}!`, canvas.width / 2.7, canvas.height / 1.8);
+    welcomeText(ctx, canvas);
+    userText(ctx, canvas, member);
+    memberCount(ctx, canvas, member)
+    avatarImg(ctx, canvas, member);
 
-    ctx.font = '24px sans-serif'
-    ctx.fillStyle = canvasTheme.color
-    ctx.fillText(`MemberCount: ${member.guild.memberCount}`, canvas.width / 2.7, canvas.height / 1.3);
-
-    ctx.strokeStyle = canvasTheme.color
-    ctx.lineWidth = 6
-    ctx.beginPath()
-    ctx.arc(canvas.height / 2, canvas.height / 2, canvas.height / 2.5, 0, Math.PI * 2, true);
-    ctx.closePath()
-    ctx.clip()
-
-    ctx.drawImage(avatar, 25, 25, 200, 200)
     return new MessageAttachment(canvas.toBuffer(), 'welcome.png')
 }
 
@@ -82,8 +103,8 @@ export async function welcomeImage(member: GuildMember, theme = 'sakura') {
  * @param {string} theme Theme of the card, this is optional
  */
 export async function goodbyeImage(member: GuildMember, theme = 'sakura') {
-    let canvasTheme = themeArray.find(t => t.name === theme.toLowerCase())
-    if (!canvasTheme) throw 'Invalid theme! Use: ' + themeArray.map(v => v.name).join(' | ');
+    let canvasTheme = themes.find(t => t.name === theme.toLowerCase())
+    if (!canvasTheme) throw 'Invalid theme! Use: ' + themes.map(v => v.name).join(' | ');
 
     const canvas = createCanvas(700, 250)
     const ctx = canvas.getContext('2d')
@@ -91,26 +112,16 @@ export async function goodbyeImage(member: GuildMember, theme = 'sakura') {
     const background = await theme2Img(theme);
     const avatar = await loadImage(member.user.displayAvatarURL({ format: 'png' }))
 
-    ctx.drawImage(background, 0, 0)
-
+    ctx.drawImage(background, 0, 0);
     ctx.strokeRect(0, 0, canvas.width, canvas.height);
 
-    ctx.font = '30px sans-serif';
-    ctx.fillStyle = canvasTheme.color
-    ctx.fillText(`Goodbye,`, canvas.width / 2.7, canvas.height / 3.5);
+    ctx.fillStyle = canvasTheme.color;
+    ctx.strokeStyle = canvasTheme.color;
 
-    ctx.font = `${getFontSize(member.user.tag)}px sans-serif`
-    ctx.fillStyle = canvasTheme.color
-    ctx.fillText(`${member.user.tag}!`, canvas.width / 2.7, canvas.height / 1.8);
+    goodbyeText(ctx, canvas);
+    userText(ctx, canvas, member);
+    avatarImg(ctx, canvas, member);
 
-    ctx.strokeStyle = canvasTheme.color
-    ctx.lineWidth = 6
-    ctx.beginPath()
-    ctx.arc(canvas.height / 2, canvas.height / 2, canvas.height / 2.5, 0, Math.PI * 2, true);
-    ctx.closePath()
-    ctx.clip()
-
-    ctx.drawImage(avatar, 25, 25, 200, 200)
     return new MessageAttachment(canvas.toBuffer(), 'goodbye.png')
 }
 
