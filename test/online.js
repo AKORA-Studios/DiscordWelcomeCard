672aac3c894875b976e7823471936788b8058199
AKORA <dragoncatm@gmail.com>
2023-03-27 16:10:55 +0200

    Update deps

---
+++ test/online.js
@@ -0,0 +1,62 @@
+const { Client, GatewayIntentBits } = require('discord.js');
+const { join } = require('path');
+const { writeFileSync: write, readFileSync, writeFileSync } = require('fs');
+const { Text, drawCard, LinearGradient } = require('../');
+const path = (s) => join(__dirname, s);
+
+const token = readFileSync(path('../.env')).toString();
+
+const client = new Client({ intents: [] });
+
+const grad = new LinearGradient(
+  {
+    color: '#f00',
+    offset: 0,
+  },
+  { color: '#00f', offset: 1 }
+);
+
+/** @type {(url: string) => import('../').CardOptions} */
+const op = (url) => ({
+  avatar: {
+    outlineWidth: 10,
+    outlineColor: grad,
+    image: url,
+    imageRadius: 0.8,
+    borderRadius: 0.5,
+  },
+  card: {
+    blur: 4,
+    border: false,
+    rounded: false,
+    background: readFileSync(path('images/wallpaper.png')),
+  },
+  generation: { static: 'Card1', format: 'image/jpeg' },
+  theme: 'code',
+  text: {
+    color: '#1add89',
+    title: new Text('humongus title', 240, 70).setFontSize(55).setStyle('#fff').stroke(),
+    text: 'Some EMojis☑️❌🇮🇶',
+    subtitle: new Text('YourFancy long humongus giant horse text is is´nsalnel long', 240, 180)
+      .setFontSize(30)
+      .setGradient(grad)
+      .multiline(),
+    //.setRect(400, 50),
+  },
+});
+
+client.on('ready', async () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+  let user = await client.users.fetch('387655649934311427');
+
+  let url = user.displayAvatarURL({
+    extension: 'png',
+  });
+
+  console.log(url);
+
+  let buff = await drawCard(op(url));
+  writeFileSync(path('images/custom_full.png'), buff);
+});
+
+client.login(token);
